90682ecc488f9d921893cc17eca6fe2fed57891d
fengkx <liangkx8237@gmail.com>
2018-12-24 19:51:17 +0800

    query SQLite before request to testUrl

--- database/initTables.js
+++ database/initTables.js
@@ -5,7 +5,7 @@ const logger = require('../utils/logger');
 const dbPromise = require('./index');
 const initTables = async () => {
     if (!fs.existsSync(__dirname + '/sql/create_tables.sql')) {
-        fs.openSync(__dirname + '/sql/create_tables.sql', 'w');
+        throw new Error('CAN_INIT_DB');
     }
     const sql = fs.readFileSync(__dirname + '/sql/create_tables.sql', {
         encoding: 'utf-8'
--- middlewares/testUrl.js
+++ middlewares/testUrl.js
@@ -1,25 +1,34 @@
 const axios = require('axios');
 const Parser = require('rss-parser');
+const RSS = require('../proxies/rssFeed');
+
 module.exports = async (ctx, next) => {
     const url = encodeURI(ctx.state.feedUrl);
-    try {
-        const res = await axios.get(url);
-        const parser = new Parser();
-        let feed = await parser.parseString(res.data);
-        delete feed.items;
+    const feed = await RSS.getFeedByUrl(url);
+    if (!!feed) {
         ctx.state.feed = feed;
-    } catch (e) {
-        if (!!e.respone) {
-            switch (e.respone.status) {
-                case 404:
-                case 403:
-                    throw new Error(e.respone.status);
-                default:
-                    throw new Error('FETCH_ERROR');
+        ctx.state.feed.title = feed.feed_title;
+        await next();
+    } else {
+        try {
+            const res = await axios.get(url);
+            const parser = new Parser();
+            let feed = await parser.parseString(res.data);
+            delete feed.items;
+            ctx.state.feed = feed;
+        } catch (e) {
+            if (!!e.respone) {
+                switch (e.respone.status) {
+                    case 404:
+                    case 403:
+                        throw new Error(e.respone.status);
+                    default:
+                        throw new Error('FETCH_ERROR');
+                }
+            } else {
+                throw new Error('FETCH_ERROR');
             }
-        } else {
-            throw new Error('FETCH_ERROR');
         }
+        await next();
     }
-    await next();
 };
