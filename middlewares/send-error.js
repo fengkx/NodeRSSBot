35869bff8ec11377db112d14cd632941bc20ae04
fengkx <liangkx8237@gmail.com>
2019-06-15 20:20:49 +0800

    fix id different not found when callback

--- middlewares/send-error.js
+++ middlewares/send-error.js
@@ -3,7 +3,16 @@ const logger = require('../utils/logger');
 const errors = require('../utils/errors');
 const USERS = require('../proxies/users');
 module.exports = async (ctx, next) => {
-    const user = await USERS.getUserById(ctx.message.chat.id);
+    let id;
+    switch (ctx.updateType) {
+        case 'message':
+            id = ctx.message.chat.id;
+            break;
+        case 'callback_query':
+            id = ctx.callbackQuery.from.id;
+            break;
+    }
+    const user = await USERS.getUserById(id);
     if (user) ctx.state.lang = user.lang;
     const lang = ctx.state.lang || require('../config').lang;
     const m = await ctx.reply(i18n[lang]['PROCESSING']);
