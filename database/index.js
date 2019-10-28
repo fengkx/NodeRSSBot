13f49e2739651fac82bea6950f998109cf71719d
fengkx <liangkx8237@gmail.com>
2019-10-28 20:30:25 +0800

    bump version 0.3.1

--- database/index.js
+++ database/index.js
@@ -1,5 +1,6 @@
 const config = require('../config');
 const sqlite = require('sqlite');
 
+// eslint-disable-next-line object-property-newline
 const dbPromise = sqlite.open(config.db_path, { Promise, cached: true });
 module.exports = dbPromise;
