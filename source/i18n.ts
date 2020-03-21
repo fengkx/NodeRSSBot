59e83b8906d5f8cb8d305f0355a4cb292cfc818f
fengkx <liangkx8237@gmail.com>
2020-03-22 04:07:20 +0800

    type: i18n use d.ts

--- source/i18n.ts
+++ source/i18n.ts
@@ -1,8 +1,9 @@
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
 import * as path from 'path';
+import { I18n } from './types/i18n';
 
-const result = {};
+const result: I18n = {};
 const localeDir = path.join(__dirname, '../i18n'); // /dist/source/i18n -> /dist/[i18n]
 const baseStr = fs.readFileSync(path.join(localeDir, 'en.yaml'), {
     encoding: 'utf8'
