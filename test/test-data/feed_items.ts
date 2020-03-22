0381f97bc605d5b600b16b42d6fc2c5361951bce
fengkx <liangkx8237@gmail.com>
2020-03-22 17:41:14 +0800

    type: bring up Option Message FeedItem types

--- test/test-data/feed_items.ts
+++ test/test-data/feed_items.ts
@@ -1,4 +1,6 @@
-export const atom = {
+import { FeedItem } from '../../source/types/feed';
+
+export const atom: FeedItem = {
     link: 'https://www.v2ex.com/t/611572#reply218',
     title: '采取 RESTful 风格的 api 是否应该对结果包一层？',
     content:
@@ -6,14 +8,14 @@ export const atom = {
     id: 'tag:www.v2ex.com,2019-10-21:/t/611572'
 };
 
-export const rss = {
+export const rss: FeedItem = {
     link: 'https://toutiao.io/k/dld5rnz',
     title: '不论微信钉钉还是什么软件，我写了个通用的消息监控处理机器人',
     content: '不论微信钉钉还是什么软件，我写了个通用的消息监控处理机器人',
     guid: 'https://toutiao.io/k/dld5rnz'
 };
 
-export const noKey = {
+export const noKey: FeedItem = {
     link: 'https://toutiao.io/k/dld5rnz',
     title: '不论微信钉钉还是什么软件，我写了个通用的消息监控处理机器人',
     content: '不论微信钉钉还是什么软件，我写了个通用的消息监控处理机器人'
