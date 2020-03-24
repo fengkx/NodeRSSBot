bca815a68745feaf154c0077ce967222deeed008
fengkx <liangkx8237@gmail.com>
2020-03-25 00:40:36 +0800

    use got-iconv

--- source/utils/got.ts
+++ source/utils/got.ts
@@ -1,9 +1,5 @@
-import got from 'got';
+import got from 'got-iconv';
 import { config } from '../config';
-import charset from 'charset';
-import * as charDet from 'jschardet';
-import { decode } from 'iconv-lite';
-import { Response } from 'got/dist/source/types';
 import agent from './agent';
 
 const AcceptHeader =
@@ -20,30 +16,4 @@ const custom = got.extend({
     agent: agent
 });
 
-export default custom.extend({
-    hooks: {
-        afterResponse: [
-            async (res: Response) => {
-                let fromHeader = false;
-                let enc = charset(res.headers['content-type']);
-                if (enc) {
-                    fromHeader = true;
-                } else {
-                    enc = charDet.detect(res.body as Buffer).encoding;
-                }
-                if (enc !== 'utf8') {
-                    (res.body as Buffer) = await custom(res.url).buffer();
-                    if (!fromHeader) {
-                        enc =
-                            charDet.detect(res.body as Buffer).encoding ||
-                            'utf8';
-                        enc = enc.toLowerCase();
-                    }
-                    res.body = decode(res.body as Buffer, enc);
-                }
-
-                return res;
-            }
-        ]
-    }
-});
+export default custom;
