27771596aebda4c8756f1e163f3cfab11e41492d
fengkx <liangkx8237@gmail.com>
2020-03-27 11:01:16 +0800

    dep: bump got-iconv

--- source/utils/got.ts
+++ source/utils/got.ts
@@ -1,11 +1,12 @@
 import got from 'got-iconv';
+import { Got } from 'got-iconv';
 import { config } from '../config';
 import agent from './agent';
 
 const AcceptHeader =
     'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8 ';
 
-const custom = got.extend({
+const custom = ((got as unknown) as Got).extend({
     headers: {
         'user-agent': config.UA,
         accept: AcceptHeader
