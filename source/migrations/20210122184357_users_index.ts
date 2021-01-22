b95a9b0f3279f4d645812ed210f34b53057fbdbe
fengkx <liangkx8237@gmail.com>
2021-01-22 18:48:06 +0800

    fix: add users user_id unique index

---
+++ source/migrations/20210122184357_users_index.ts
@@ -0,0 +1,15 @@
+import * as Knex from 'knex';
+
+export async function up(knex: Knex): Promise<void> {
+    knex.schema.alterTable('users', function (t) {
+        t.unique(['user_id']);
+        t.index(['user_id', 'lang']);
+    });
+}
+
+export async function down(knex: Knex): Promise<void> {
+    knex.schema.alterTable('users', function (t) {
+        t.dropUnique(['user_id']);
+        t.dropIndex(['user_id', 'lang']);
+    });
+}
