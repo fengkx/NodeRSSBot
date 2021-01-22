fc242fe2d9fe17c4371b769df1638b6ff687b38c
fengkx <liangkx8237@gmail.com>
2021-01-22 19:32:38 +0800

    fix: alertTable

--- source/migrations/20210122184357_users_index.ts
+++ source/migrations/20210122184357_users_index.ts
@@ -1,14 +1,14 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-    knex.schema.alterTable('users', function (t) {
+    return knex.schema.alterTable('users', function (t) {
         t.unique(['user_id']);
         t.index(['user_id', 'lang']);
     });
 }
 
 export async function down(knex: Knex): Promise<void> {
-    knex.schema.alterTable('users', function (t) {
+    return knex.schema.alterTable('users', function (t) {
         t.dropUnique(['user_id']);
         t.dropIndex(['user_id', 'lang']);
     });
