a21b3bbcf087d34ca91b840cb51379cc1f643bf7
fengkx <liangkx8237@gmail.com>
2022-02-08 23:18:12 +0800

    chore: update dependencies

--- source/knexfile.ts
+++ source/knexfile.ts
@@ -5,18 +5,20 @@ import { Knex } from 'knex';
 import { join } from 'path';
 import logger from './utils/logger';
 
+const SQLITE_CLIENT = 'sqlite';
+
 const parsed = parse(config.db_path);
 const isWindows = process && process.platform && process.platform === 'win32';
 let { protocol } = parsed,
     client;
 const isDriveLetter = isWindows && protocol && protocol.length === 2;
 if (protocol === null || isDriveLetter) {
-    client = 'sqlite';
+    client = SQLITE_CLIENT;
 } else if (protocol.slice(-1) === ':') {
     protocol = protocol.slice(0, -1);
     client = protocol;
 }
-// console.log(config.db_path);
+
 const knexConfig: Knex.Config = {
     client,
     connection: config.db_path,
@@ -27,10 +29,10 @@ const knexConfig: Knex.Config = {
     },
     pool: {
         min: 1,
-        max: client === 'sqlite' ? 4 : 8,
+        max: client === SQLITE_CLIENT ? 4 : 8,
         acquireTimeoutMillis: 1000 * 5 * 60, // timeout 5 minutes
         afterCreate: (conn: any, done: (err: Error, conn: any) => void) => {
-            if (client === 'sqlite') {
+            if (client === SQLITE_CLIENT) {
                 conn.run('PRAGMA foreign_keys = ON;', function (err) {
                     done(err, conn);
                 });
